501963cf660d7e81c25f283791bc871b7b6477e3
Mayuresh <mayureshkn99@gmail.com>
2021-12-18 01:47:34 +0530

    Added broadcast for server ip Added listener for client Added client handler Created players list

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFlatButton, MDFillRoundFlatButton
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.toast import toast
+import socket
+import threading
+import random
+
 
 Window.size = (350, 650)
 Window.softinput_mode = "below_target"
 HEADER = 64
 FORMAT = 'utf-8'
 DISCONNECT = "DISCONNECT"
+STOP = False
 PLAYERS = []
 ROLES = ["Chor", "Police"]
 
@@ -26,6 +31,12 @@ class MyButton(MDFillRoundFlatButton):
 class MyBoxLayout(MDBoxLayout):
     """Custom Definition of BoxLayout"""
     pass
+
+class Player():
+
+    def __init__(self, conn, addr):
+        self.conn = conn
+        self.addr = addr
 
 
 class HomePage(MDScreen):
@@ -48,6 +59,62 @@ class LobbyPage(MDScreen):
         self.host_name = self.manager.get_screen("HostPage").ids.host_name.text
         self.ids.lobby_name.text = self.host_name + "'s Lobby"
 
+    def broadcast(self):
+        global STOP
+
+        # Initializing broadcast socket
+        print("Started Broadcast")
+        broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Broadcast IP address and Username Continuously
+        while True:
+            message = (socket.gethostbyname(socket.getfqdn()) + ' ' + self.host_name).encode('utf-8')
+            broadcast_socket.sendto(message, ('<broadcast>', 8888))
+            if STOP:
+                broadcast_socket.close()
+                print("Stopped Broadcast")
+                return
+
+    def host(self):
+        # Broadcast server IP
+        threading.Thread(target=self.broadcast).start()
+
+        # Initializing server object with ip '' and port 9999
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.settimeout(None)
+        server.bind(('', 9999))
+
+        # Server listens for client connections
+        server.listen()
+        print("Listening...")
+        while True:
+            conn, addr = server.accept()
+            if addr == socket.gethostbyname(socket.gethostname()):
+                return  # Stop listening if received own addr
+            conn.settimeout(None)
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print("Active Connections: ", threading.activeCount() - 2)
+
+    def handle_client(self, conn, addr):
+        global PLAYERS
+        player = Player(conn, addr)
+        print(addr, "connected")
+        while True:
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT:
+                    PLAYERS.remove(next(x for x in PLAYERS if x.addr == addr))
+                    break
+                print("<", addr, ">", msg)
+                player.username = msg
+                PLAYERS.append(player)
+        conn.close()
 
 class JoinPage(MDScreen):
 
